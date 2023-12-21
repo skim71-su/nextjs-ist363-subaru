b2f028516dc9effaa3a9603f8816d0de2d9542b1
skim71-su <112636203+skim71-su@users.noreply.github.com>
2023-12-21 12:47:42 -0500

    final project completed

--- components/ColorPicker.js
+++ components/ColorPicker.js
@@ -8,7 +8,7 @@ import styles from './colorpicker.module.scss';
 import Button from './Button';
 import ButtonGroup from './ButtonGroup';
 import Container from './Container';
-import Paragraph from './Paragraph';
+import Link from 'next/link';
 
 
 const ColorPicker = ({ colors, vehicleName }) => {
@@ -30,6 +30,30 @@ const ColorPicker = ({ colors, vehicleName }) => {
           activeColor={activeColor}
         />
         <Heading level={3} textAlign="center">{colors[activeColor].swatch.edges[0].node.name}</Heading>
+        <Container>
+            <div className={styles.colorpicker__calltoactionsreg}>
+              <div className={styles.calltoactionsreg__flexbox}>
+                  <Heading level={2}>Get Behind the Wheel</Heading>
+                  <p className={styles.colorpicker__calltoactionsreg__paragraph}>
+                      Start customizing your {vehicleName} today, or contact your local Subaru dealer to experience one in person.
+                  </p>
+                  <ButtonGroup>
+                      <Link href="/vbuild-price">
+                          <Button type="primary">
+                            Build & Price
+                          </Button>
+                      </Link>
+                      <Link href="/customer-support">
+                          <Button type="secondary">
+                            Contact Dealer
+                          </Button>
+                      </Link>
+                  </ButtonGroup>
+              </div>
+            </div>
+            
+        </Container>
+    
         
         
         
